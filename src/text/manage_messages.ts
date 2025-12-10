e5521d6e74647d70c3e484fe05b3fc11f5d3e2dc
Firebone <blackrubin@gmail.com>
2025-12-10 15:29:22 +0100

    code cleanup

--- src/text/manage_messages.ts
+++ src/text/manage_messages.ts
@@ -11,45 +11,64 @@ const replyToMessage = (ctx: Context, messageId: number, string: string) =>
     reply_parameters: { message_id: messageId },
   });
 
-const manageMessages = () => async (ctx: Context) => {
-  debug('Triggered "manageMessages" text command');
-
-  const messageId = ctx.message?.message_id;
-  const messageText = ctx.text;
-  const userName = `${ctx.message?.from.first_name}`;
-
-  //if the message is a reply and it is from admin, send a message to the original sender
+const checkAdminMessage = (ctx: Context) => {
   if (ctx.chat?.id.toString() === ADMIN_CHAT_ID) {
     let message = ctx.message;
-    if (message && ("reply_to_message" in message) ) {
+    if (message && ("reply_to_message" in message)) {
       let replymessage = message.reply_to_message;
       //const originalSenderId = JSON.stringify(message.reply_to_message);
       //console.log(originalSenderId);
       let replyText = ctx.text || '';
       //const replyToMessageId = message.reply_to_message?.message_id || '';
       let originalSenderId = 0;
-      if(replymessage && ("forward_origin" in replymessage) ) {
+      if (replymessage && ("forward_origin" in replymessage)) {
         let fwOrigin = replymessage.forward_origin;
-        if(fwOrigin && ("sender_user" in fwOrigin) ) {
+        if (fwOrigin && ("sender_user" in fwOrigin)) {
           originalSenderId = fwOrigin.sender_user.id;
         }
       }
       console.log(`Admin replied to message from user ID ${originalSenderId} with text: ${replyText}`);
       //ctx.sendMessage(originalSenderId || 0, `Admin reply: ${replyText}`);
-      await ctx.telegram.sendMessage(originalSenderId || 0, `*Admin reply*: ${replyText}`, {
-            parse_mode: 'MarkdownV2',
-            reply_parameters: { message_id: messageId || 0 }
-        }).then(() => {
-            console.log("Message sent to original sender");
-        }).catch((error) => {
-            console.error("Error sending message to original sender:", error);
-        });
-      return;
+      ctx.telegram.sendMessage(originalSenderId || 0, `*Admin reply*: ${replyText}`, {
+        parse_mode: 'MarkdownV2'
+        //,reply_parameters: { message_id: messageId || 0 }
+      }).then(() => {
+        console.log("Message sent to original sender");
+      }).catch((error) => {
+        console.error("Error sending message to original sender:", error);
+      });
+      return true;
     }
+  }
+  return false;
+};
+
+const manageMessages = () => async (ctx: Context) => {
+  debug('Triggered "manageMessages" text command');
+
+  //skip if from bot
+  if (ctx.message?.from?.is_bot) {
+    console.info(`Ignoring message from bot user: ${ctx.message.from.username}`);
+    return;
+  }
+
+  //if the message is a reply and it is from admin, send a message to the original sender
+  let isAdminMessage = checkAdminMessage(ctx);
+  if (isAdminMessage) {
+    return;
+  }
+
+  const messageId = ctx.message?.message_id;
+  var messageText = ctx.text;
+  const userName = `${ctx.message?.from.first_name}`;
+
+  //remove @RPGdiceBot from text message
+  if (messageText && messageText.endsWith('@RPGdiceBot')) {
+    messageText = messageText.replace('@RPGdiceBot', '').trim();
   }
 
   //check if it is a roll command or message has roll syntax (roll syntax may start with "/") 
-  if(messageText?.toLowerCase().startsWith('roll ') || messageText?.toLowerCase().startsWith('r ') || /^(\/)?(\d+)?d(\d+)(d(\d+))?$/i.test(messageText || '')) {
+  if (messageText && messageText?.toLowerCase().startsWith('roll ') || messageText?.toLowerCase().startsWith('r ') || /^(\/)?(\d+)?d(\d+)(d(\d+))?$/i.test(messageText || '')) {
     await roll()(ctx);
     return;
   }
