d972db5507acb7bf9a9669fd072b74ec7b54586a
Firebone <blackrubin@gmail.com>
2025-12-10 09:56:08 +0100

    adding debug function

--- src/commands/debug.ts
+++ src/commands/debug.ts
@@ -4,14 +4,17 @@ import createDebug from 'debug';
 const debug = createDebug('bot:debug_command');
 
 const debugFn = () => async (ctx: Context) => {
+  debug(`Triggered "debug" command.`);
   if(ctx.message?.from.username !== 'firebone') {
     //await ctx.reply('You are not authorized to use this command (' + ctx.message?.from.username + ')');
     return;
   }
   //transform ctx to string
-  const message = JSON.stringify(ctx, null, 2);
-  debug(`Triggered "debug" command.`);
+  
+  const message = JSON.stringify(ctx.from, null, 2);
   await ctx.reply(message);
+  const message2 = JSON.stringify(ctx.chat, null, 2);
+  await ctx.reply(message2);
 };
 
 export { debugFn };
